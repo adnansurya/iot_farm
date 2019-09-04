a4bd64b6d87e01d97b0b66d44cf4a27c1d3f4267
Unknown <pc.keyboardist@gmail.com>
2019-09-05 03:09:27 +0800

    create index page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Blank Page | Bootstrap Based Admin Template - Material Design</title>
+    <?php require('partials/head.php'); ?>
+</head>
+
+<body class="theme-green">
+    <!-- Page Loader -->
+    <?php require('partials/page_loader.php'); ?>
+    <!-- #END# Page Loader -->
+
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+
+    <!-- Top Bar -->
+    <?php require('partials/top_bar.php'); ?>
+    <!-- #Top Bar -->
+
+    <section>
+        <!-- Left Sidebar -->
+        <?php require('partials/left_sidebar.php'); ?>
+        <!-- #END# Left Sidebar -->        
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>BLANK PAGE</h2>
+            </div>
+        </div>
+    </section>
+
+    <?php require('partials/scripts.php'); ?>
+</body>
+
+</html>
--- partials/top_bar.php
+++ partials/top_bar.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="navbar-header">           
             <a href="javascript:void(0);" class="bars"></a>
-            <a class="navbar-brand" href="index.html">ADMINBSB - MATERIAL DESIGN</a>
+            <a class="navbar-brand" href="index.php">ADMINBSB - MATERIAL DESIGN</a>
         </div>
        
     </div>
