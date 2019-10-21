b0f05aefa7f8b6dcf34c763cff5e47368797990e
Adnan <muh.adnansuryaazis@gmail.com>
2019-10-21 12:05:05 +0800

    update value with ajax

--- settings.php
+++ settings.php
@@ -35,14 +35,56 @@
                     <div class="card">
                         <div class="header">
                             <h2>
-                               PENYIRAMAN OTOMATIS
+                                PENYIRAMAN OTOMATIS
                             </h2>                           
                         </div>
                         <div class="body">
-                             <label>
-                                <input type="checkbox" id="pompaSw" />
+                        <div class="row">
+                            <label>
+                                <input type="checkbox" id="pompaSw">
                                 <span>Aktif</span>
-                              </label>
+                            </label>
+                        </div>
+                            <div class="row">
+                                <div class="col-md-6">  
+                                    <p>Suhu Udara (Celcius)</p>                              
+                                    <input id="suhuUdaraTxt" type="number" min="0" max="59"/>
+                                </div>                                 
+                                <div class="col-md-6">
+                                    <p>Kelembaban Udara (%)</p>
+                                    <input id="lembabUdaraTxt" type="number" min="0" max="59"/>
+                                </div> 
+                                <div class="col-md-12">
+                                    <p>Kelembaban Tanah</p>
+                                    <input id="lembabTanahTxt" type="number" min="0" max="59"/>
+                                </div>                              
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                               PERIODE UPDATE DATA
+                            </h2>                           
+                        </div>
+                        <div class="body">
+                        <div class="row">
+                                <div class="col-sm-3">
+                                   <input id="angkaTxt" type="number" min="0" max="59"/>
+                                </div>
+                                <div class="col-sm-9">
+                                    <select id="waktuSel" class="form-control show-tick">
+                                        <option value="" disabled>-- Please select --</option>
+                                        <option value="sec">Detik</option>
+                                        <option value="min">Menit</option>
+                                        <option value="hour">Jam</option>
+                                        <option value="day">Hari</option>
+                                        <option value="month">Bulan</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -51,23 +93,25 @@
     </section>
 
     <?php require('partials/scripts.php'); ?>
+    <?php require('api/db_access.php'); ?>
+    <?php 
+    
+        function get_value($koneksi, $params){
+            $parameter = $params;
+            $result = mysqli_query($koneksi,"SELECT * FROM iot_farm_kontrol WHERE params='" .$params. "'");
+            $row = mysqli_fetch_assoc($result);
+            return $row['value'];
+        }
+    ?>
     <script>
-        $(function() {
-  // hooking event only on buttons, can do tr's as well.
-            $('#pompaSw').click(function(){
-                let params = 'penyiraman';
-                let switch_;
-                if($(this).prop('checked')){
-                    switch_ = 'ON';
-                }else{
-                    switch_ = 'OFF';
-                }
-                $.ajax({
+
+        function updateAJAX(params, value){
+            $.ajax({
                 url: 'api/update_settings.php',
                 type: 'post',
                 data: {
                     params : params,
-                    value: switch_
+                    value: value
                 },
                
                 success: function(response){
@@ -86,6 +130,55 @@
                              
                 }
                 });
+        }
+        $(function() {
+            $("#angkaTxt").val('<?php echo get_value($conn, 'angka_periode'); ?>');
+            $("#angkaTxt").change(function(){ 
+                let angka = $('#angkaTxt').val();
+                updateAJAX('angka_periode', angka);
+            });       
+            
+            $("#waktuSel").val('<?php echo get_value($conn, 'waktu_periode'); ?>');
+            $("#waktuSel").change(function(){                
+                let waktu = $('#waktuSel').val();
+                updateAJAX('waktu_periode', waktu);                
+            });
+
+            $("#suhuUdaraTxt").val('<?php echo get_value($conn, 'limit_suhu'); ?>');
+            $("#suhuUdaraTxt").change(function(){                
+                let suhu = $('#suhuUdaraTxt').val();
+                updateAJAX('limit_suhu', suhu);                
+            });
+
+            $("#lembabUdaraTxt").val('<?php echo get_value($conn, 'limit_lembab_udara'); ?>');
+            $("#lembabUdaraTxt").change(function(){                
+                let lembab_udara = $('#lembabUdaraTxt').val();
+                updateAJAX('limit_lembab_udara', lembab_udara);                
+            });
+
+            $("#lembabTanahTxt").val('<?php echo get_value($conn, 'limit_lembab_tanah'); ?>');
+            $("#lembabTanahTxt").change(function(){                
+                let lembab_tanah = $('#lembabTanahTxt').val();
+                updateAJAX('limit_lembab_tanah', lembab_tanah);                
+            });
+
+            let pompaStat = '<?php echo get_value($conn, 'penyiraman'); ?>';
+            if(pompaStat == 'ON'){
+                $('#pompaSw').prop('checked', true);
+            }else{
+                $('#pompaSw').prop('checked', false);
+            }
+            console.log(pompaStat);
+            
+            $('#pompaSw').click(function(){                
+                let switch_;
+                if($(this).prop('checked')){
+                    switch_ = 'ON';
+                }else{
+                    switch_ = 'OFF';
+                }
+                updateAJAX('penyiraman', switch_);
+                
             });
         });
     </script>
